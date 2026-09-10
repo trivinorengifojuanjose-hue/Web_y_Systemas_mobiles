17c144810d36db99a5f9fe16d56b03cba3400164
Juant188 <trivinorengifojuanjose@gmail.com>
2026-09-10 01:56:11 +0000

    commit final

--- App/main.py
+++ App/main.py
@@ -1,8 +1,35 @@
+# import json
+#
+# with data_file.open('r') as file:
+#     clientes = json.load(file)
+#
+# for cliente in clientes:
+#     print(cliente["nombre"])
 import json
+from fastapi import FastAPI
 
-with data_file.open('r') as file:
-    clientes = json.load(file)
+app = FastAPI()
 
-for cliente in clientes:
-    print(cliente["nombre"])
- 
+@app.get("/")
+def inicio():
+    return {"message": "API"}
+
+@app.get("/clientes")
+def obtener_clientes():
+
+   with     open("data/clientes.json", "r") as file:
+        clientes = json.load(file)
+
+    return clientes
+
+    
+@app.get("/clientes/{cliente_id}")
+def obtener_cliente(cliente_id: int):
+    with open("data/clientes.json", "r") as file:
+        clientes = json.load(file)
+
+    for cliente in clientes:
+        if cliente["id"] == cliente_id:
+            return cliente
+
+    return {"message": "Cliente no encontrado"}
